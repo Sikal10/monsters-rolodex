890d245b644f951a1ddd4d2c755405fa630d4db7
Sikal <sikal.sikal@yahoo.com>
2019-11-09 22:14:13 +0000

    Updates

---
+++ precache-manifest.d857e89d917347f211bf0558d076bb54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6229415b298445d344ace9f8a962dff",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "245f1f298e00eb9a085d",
+    "url": "/monsters-rolodex/static/css/main.811b8ac7.chunk.css"
+  },
+  {
+    "revision": "c4de08c8e1140d25c0ae",
+    "url": "/monsters-rolodex/static/js/2.5905bb17.chunk.js"
+  },
+  {
+    "revision": "245f1f298e00eb9a085d",
+    "url": "/monsters-rolodex/static/js/main.ae13a30c.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
